439bd44c4d2e8b33c422a1eb08ff422e98d269af
Anderson <andersoncleyson@protonmail.com>
2026-05-14 08:23:59 -0300

    Tipos de dados

---
+++ Aula3/tipos_dados.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+unsigned long quadrado(long base) {
+    return base * base;
+}
+
+int main(void){
+    long num = 100000;
+
+    printf("%ld^2 = %ld\n", num, quadrado(num));
+
+    return 0;
+}
---
+++ Aula8/opbit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 8;
+    int b = 4;
+
+    printf("O resultado é %d\n", a & b);
+
+    return 0;
+}
